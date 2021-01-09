1a031abc454263fb989c7823d4dd31f3feede647
Krysta Hunt <kdhunt@musefish.net>
2021-01-08 21:36:28 -0700

    portal page

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
     if (document.readyState === "interactive" || document.readyState === "complete" ) {
         document.getElementById('copydt').innerText = current_date.getFullYear();
         document.getElementById('updatedt').innerText = 
-        current_date.getMonth()+1
-        +"/"+current_date.getDate()
+        ("0" + current_date.getMonth()+1).slice(-2)
+        +"/"+("0" + current_date.getDate()).slice(-2)
         +"/"+current_date.getFullYear()
         +" "+current_date.getHours()
         +":"+current_date.getMinutes()
