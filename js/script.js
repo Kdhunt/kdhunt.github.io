9e64f6c0525184201e8350b7c6f446f249b7c01e
Krysta Hunt <kdhunt@musefish.net>
2021-01-08 23:12:51 -0700

    update

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ document.getElementById('copydt').innerText = current_date.getFullYear();
         +"/"+("0" + current_date.getDate()).slice(-2)
         +"/"+current_date.getFullYear()
         +" "+current_date.getHours()
-        +":"+current_date.getMinutes()
-        +":"+current_date.getSeconds()
+        +":"+("0" + current_date.getMinutes()+1).slice(-2)
+        +":"+("0" + current_date.getSeconds()+1).slice(-2)
         ;
     }
 });
