f2734565fbcad66618f3b6051c806cb3c59d3382
Krysta Hunt <kdhunt@musefish.net>
2021-01-08 21:26:57 -0700

    portal page

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var current_date = new Date();
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    if (document.readyState === "interactive" || document.readyState === "complete" ) {
+        document.getElementById('copydt').innerText = current_date.getFullYear();
+        document.getElementById('updatedt').innerText = 
+        current_date.getMonth()+1
+        +"/"+current_date.getDate()
+        +"/"+current_date.getFullYear()
+        +" "+current_date.getHours()
+        +":"+current_date.getMinutes()
+        +":"+current_date.getSeconds()
+        ;
+        document.getElementById('updatedt').innerText = current_date.now();
+    }
+});
+
+
