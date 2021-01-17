9a7eb1d5f0c429d68de269962b639806af513d0e
Krysta Hunt <kdhunt@musefish.net>
2021-01-16 19:46:32 -0700

    link update

---
+++ lesson2/js/script.js
@@ -0,0 +1,36 @@
+var current_date = new Date();
+/*document.getElementById('copydt').innerText = current_date.getFullYear();
+    document.getElementById('updatedt').innerText = 
+    ("0" + current_date.getMonth()+1).slice(-2)
+    +"/"+("0" + current_date.getDate()).slice(-2)
+    +"/"+current_date.getFullYear()
+    +" "+current_date.getHours()
+    +":"+current_date.getMinutes()
+    +":"+current_date.getSeconds()
+    ;
+*/
+/*document.addEventListener("DOMContentLoaded", () => {
+    console.log(document.readyState);
+    if (document.readyState === "interactive" || document.readyState === "complete" ) {
+        document.getElementById('copydt').innerText = current_date.getFullYear();
+        document.getElementById('updatedt').innerText = 
+        ("0" + current_date.getMonth()+1).slice(-2)
+        +"/"+("0" + current_date.getDate()).slice(-2)
+        +"/"+current_date.getFullYear()
+        +" "+current_date.getHours()
+        +":"+("0" + current_date.getMinutes()+1).slice(-2)
+        +":"+("0" + current_date.getSeconds()+1).slice(-2)
+        ;
+    }
+});
+*/
+
+    document.getElementById('copydt').innerText = current_date.getFullYear();
+    document.getElementById('updatedt').innerText = 
+    ("0" + current_date.getMonth()+1).slice(-2)
+    +"/"+("0" + current_date.getDate()).slice(-2)
+    +"/"+current_date.getFullYear()
+    +" "+current_date.getHours()
+    +":"+current_date.getMinutes()
+    +":"+current_date.getSeconds()
+    ;
