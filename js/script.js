e6c1a94e38880b6efbb5d512c355a5c6b042c3a9
Krysta Hunt <kdhunt@musefish.net>
2021-01-16 19:43:23 -0700

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var current_date = new Date();
-document.getElementById('copydt').innerText = current_date.getFullYear();
-        document.getElementById('updatedt').innerText = 
-        ("0" + current_date.getMonth()+1).slice(-2)
-        +"/"+("0" + current_date.getDate()).slice(-2)
-        +"/"+current_date.getFullYear()
-        +" "+current_date.getHours()
-        +":"+current_date.getMinutes()
-        +":"+current_date.getSeconds()
-        ;
-
+/*document.getElementById('copydt').innerText = current_date.getFullYear();
+    document.getElementById('updatedt').innerText = 
+    ("0" + current_date.getMonth()+1).slice(-2)
+    +"/"+("0" + current_date.getDate()).slice(-2)
+    +"/"+current_date.getFullYear()
+    +" "+current_date.getHours()
+    +":"+current_date.getMinutes()
+    +":"+current_date.getSeconds()
+    ;
+*/
 /*document.addEventListener("DOMContentLoaded", () => {
     console.log(document.readyState);
     if (document.readyState === "interactive" || document.readyState === "complete" ) {
@@ -25,4 +25,14 @@ document.getElementById('copydt').innerText = current_date.getFullYear();
 });
 */
 
-
+document.onload = () => {
+    document.getElementById('copydt').innerText = current_date.getFullYear();
+    document.getElementById('updatedt').innerText = 
+    ("0" + current_date.getMonth()+1).slice(-2)
+    +"/"+("0" + current_date.getDate()).slice(-2)
+    +"/"+current_date.getFullYear()
+    +" "+current_date.getHours()
+    +":"+current_date.getMinutes()
+    +":"+current_date.getSeconds()
+    ;
+}
