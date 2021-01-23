ac29e915849dcbcf7c1d7de1495796fa95b40ca3
Krysta Hunt <kdhunt@musefish.net>
2021-01-23 13:38:38 -0700

    lesson 3 updates

---
+++ lesson3/js/l3script.js
@@ -0,0 +1,13 @@
+var current_date = new Date();
+    let copydt = document.getElementById('copydt');
+    copydt.innerText = current_date.getFullYear();
+    let updatedt = document.getElementById('updatedt')
+    updatedt.innerHTML =
+    ("0" + current_date.getMonth()+1).slice(-2)
+    +"/"+("0" + current_date.getDate()).slice(-2)
+    +"/"+current_date.getFullYear()
+    +" "+current_date.getHours()
+    +":"+current_date.getMinutes()
+    +":"+current_date.getSeconds()
+    ;
+
